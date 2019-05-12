3d1d453538564fea0d78eed4b24a2d76e5a6840d
Roger Lee <rmlee0003@gmail.com>
2019-05-12 19:46:43 -0400

    added foreach to set elements display none, gave active tab class of active-tab

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -45,10 +45,12 @@ class TabLink {
      const cards = document.querySelectorAll('.card');
 
     // Iterate through the NodeList setting the display style each one to 'none'
-     //cards.forEach(element )
+     cards.forEach(element => {
+      element.style.display = "none";
+     });
     
     // Add a class of ".active-tab" to this.tabElement
-    // this.tabElement;
+     this.tabElement.add('active-tab');
   
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class. Just un-comment the code and study what is happening here.
     // this.cards.forEach(card => card.selectCard());
